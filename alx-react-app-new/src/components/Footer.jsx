f11c950a9c9d7b9e652bec18aca63a15335c67b9
Motolani <alebiosumotolani@gmail.com>
2025-11-03 22:29:17 +0100

    inline styling in Reactt

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+<footer style={{backgroundColor: 'black',fontStyle: 'italic', color: 'white', textAlign: 'center', padding: '10px'}}>
+  <p>© 2023 City Lovers</p>
+</footer>
+    );
+}
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+export default function MainContent() {
+    return (
+        <main style = {{backgroundColor: "slategray" ,fontWeight: 600, fontSize: '24px', lineHeight: '1.3', padding: '20px'}}>
+  <p> <span style= {{color: "lightblue"}}>I love to visit New York, Paris, and Tokyo.</span></p>
+</main>
+    );
+}
