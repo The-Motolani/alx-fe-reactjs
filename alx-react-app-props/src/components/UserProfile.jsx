a9452aa93982e3616bdc34d1aff6bad891142c4d
Motolani <alebiosumotolani@gmail.com>
2025-11-04 01:30:43 +0100

    refactoring prop drilling

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,14 @@
-export default function UserProfile(props) {
+import React, {useContext} from "react";
+import UserContext from "./UserContext";
+
+export default function UserProfile() {
+    const userData = useContext(UserContext);
     return (
         <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
+            <h2> {UserData.name}</h2>
+            <p>Age: {UserData.age}</p>
+            <p>Bio: {UserData.bio}</p>
+            <p>Email: {userData.email}</p>
         </div>
     );
 };
