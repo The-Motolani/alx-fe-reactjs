975cc3a95bdc8dfc371954b70a82edbe8ed29f70
Motolani <alebiosumotolani@gmail.com>
2025-11-04 01:26:30 +0100

    refactoring prop drilling

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,7 +8,7 @@ import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx'
 import ProfilePage from './components/ProfilePage.jsx'
-import UserContext from './UserContext.js'
+import UserContext from './components/UserContext.js'
 
 function App() {
   const userData = {
