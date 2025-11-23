aba8d9a73cbd4f5bdccd7de2830420f487080130
Motolani <alebiosumotolani@gmail.com>
2025-11-23 23:58:56 +0100

    Working with APIs

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,18 +1,26 @@
-import Search from "./components/Search"
+import { Routes, Route, Link } from "react-router-dom";
+import Search from "./components/Search";
 
-
-function App() {
-
+export default function App() {
   return (
-    <>
-      <div>
-        <h1 style={{ textAlign: "center"}}>
-          My Github Search App
-        </h1>
-        <Search /> 
-      </div>
-    </>
-  )
-}
+    <div className="min-h-screen bg-gray-100">
+    
+      <header className="bg-blue-600 text-white p-4">
+        <h1 className="text-xl font-bold">GitHub User Search</h1>
+      </header>
 
-export default App
+  
+      <nav className="p-4 bg-white shadow">
+        <Link className="text-blue-500 hover:underline" to="/">
+          Home
+        </Link>
+      </nav>
+
+      <main className="p-4">
+        <Routes>
+          <Route path="/" element={<Search />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
