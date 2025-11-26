1a7b2f7555b2fec7f5aed2e0a097c9dd74b99519
Motolani <alebiosumotolani@gmail.com>
2025-11-26 16:17:19 +0100

    Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
     return (
         <div className="user-profile bg-gray-100 p-8 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8">
-            <img src = "https://via.placeholder.com/150" className="rounded-full w-36 h-36  sm:w-24 sm:h-24 mx-auto" alt="User" />
+            <img src = "https://via.placeholder.com/150" className="rounded-full md:w-36 md:h-36  sm:w-24 sm:h-24 mx-auto" alt="User" />
             <h1 className="md:text-xl text-blue-800 my-4 sm:text-lg">John Doe</h1>
             <p className="text-gray-600 md:text-base sm:text-sm">Developer at Example Co. Loves to write code and explore new technologies</p>
         </div>
