d951130b98ba15efb795a604581f7c912bd8160f
Motolani <alebiosumotolani@gmail.com>
2025-12-21 10:05:39 +0100

    Advanced react routing

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Route, Switch, Link, useRouteMatch } from 'react-router-dom';
+import ProfileDetails from '../pages/ProfileDetails';
+import ProfileSettings from '../pages/ProfileSettings';
 
 
 export default function Profile() {
@@ -11,16 +13,17 @@ export default function Profile() {
         <li><Link to={`${url}/settings`}>Settings</Link></li>
       </ul>
       <Switch>
+        <Routes>
         <Route exact path={path}>
             <h3>Please select a sub-page.</h3>
         </Route>
         <Route path={`${path}/profile`}>
-        <ProfilePage />
+        <ProfileDetails />
         </Route>
         <Route path={`${path}/settings`}>
-        <ProfileSettingsPage />
+        <ProfileSettings />
         </Route>
-
+</Routes>
       </Switch>
     </div>
   )
